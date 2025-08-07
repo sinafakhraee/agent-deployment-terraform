51b0b743642bc5292c920fd27277bedef1d16a66
sinafakhraee <sina.fakhraee@gmail.com>
2025-08-07 15:08:50 -0400

    Update variables.tf

--- foundry-agent-tf/variables.tf
+++ foundry-agent-tf/variables.tf
@@ -1,7 +1,8 @@
 variable "project_endpoint" {
-  default = "https://ai-foundry-sf.services.ai.azure.com/api/projects/agent-workshop"
+  default = "https://<foundry-account>.services.ai.azure.com/api/projects/<project-name>"
 }
 
 variable "agent_spec_file" {
   default = "agent.json"   # <-- keep your JSON spec in this file
 }
+
